054c0694eadceb340ef8c35f50d0f3fe7eec1584
ilyosss <85232169+ilyosss@users.noreply.github.com>
2021-07-13 17:40:07 +0500

    Add files via upload
    input registration

---
+++ java.js
@@ -0,0 +1,61 @@
+
+
+function bb(){
+
+
+let me = prompt('Ismingiz')
+
+if(me != null){
+    console.log(me)
+}
+else{
+    console.log(me)
+}
+
+
+
+
+
+
+}
+
+
+
+
+function ii(){
+
+
+    let me = prompt('Telefon nomeringiz')
+    
+    if(me != null){
+        console.log(me)
+    }
+    else{
+        console.log(me)
+    }
+    
+    
+    
+    
+    
+    
+    }
+
+    function uu(){
+    
+    
+    let me = prompt('Manzilingiz')
+    
+    if(me != null){
+        console.log(me)
+    }
+    else{
+        console.log(me)
+    }
+    
+    
+    
+    
+    
+    
+    }
